713f7c5a6a631ca9e9a444333a7ce82a4c3d8dc9
Naveau <maximilien.naveau@gmail.com>
2018-11-13 17:00:48 +0100

    fix some ident

--- src/test_bench_8_motors/dgm_test_bench_8_motors.py
+++ src/test_bench_8_motors/dgm_test_bench_8_motors.py
@@ -4,11 +4,11 @@
 # @author Maximilien Naveau
 # @date 2018
 #
-# This file prepares the pyhton interpretor so ite contains a pointer to the
+# This file prepares the pyhton interpretor so it contains a pointer to the
 # Device and some tracers
 #
 
 def set_current(currents):
     if shape(currents) is not None:
-      robot.device.signal("ctrl_motor_currents").value = currents
+        robot.device.signal("ctrl_motor_currents").value = currents
 
