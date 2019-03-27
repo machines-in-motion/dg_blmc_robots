7159ea0a1f7a040fc37bdf7be29112fe9121d4ba
Steve Heim <heim@is.mpg.de>
2019-03-27 12:23:23 +0100

    halfway

--- demos/teststand/dg_teststand_calib2zero.py
+++ demos/teststand/dg_teststand_calib2zero.py
@@ -1,59 +1,79 @@
-## This code is a simulation for the impedance controller
+## simple jump on impact test stand
+## Author : Avadesh Meduri
+## Date : 1/03/19
 
-## Author: Avadesh Meduri
-## Date: 1/03/2019
+from leg_impedance_control.utils import *
+from leg_impedance_control.leg_impedance_controller import leg_impedance_controller
+from leg_impedance_control.traj_generators import mul_double_vec_2, scale_values, sine_generator
 
-import numpy as np
-import pinocchio as se3
-from pinocchio.utils import zero
-from dynamic_graph import plug
-
-from py_dg_blmc_robots.teststand import get_teststand_robot
-from py_dg_blmc_robots.teststand import TeststandConfig
+from dynamic_graph.sot.core.control_pd import ControlPD
 
 from dynamic_graph_manager.dg_tools import Calibrator
 from dynamic_graph.sot.core.switch import SwitchVector
-from dynamic_graph_manager.dg_tools import ControlPD
-from dynamic_graph.sot.core.operator import Stack_of_vector
-from leg_impedance_control.utils import *
 
-################################################################################
+#######################################################################################
 
-#### Simulation Setup
-# Get the robot corresponding to the quadruped.
-robot = get_teststand_robot()
+######### CALIBRATOR SETUP #############
+print("Starting up calibrator")
+leg_calibrator = Calibrator('leg_calibrator')
 
-# Define the desired position.
-q = zero(robot.pin_robot.nq)
-dq = zero(robot.pin_robot.nv)
+from dynamic_graph.sot.core.fir_filter import FIRFilter_Vector_double
+calib_vel_filtered = FIRFilter_Vector_double("calib_vel_filtered")
+filter_size = 100
+calib_vel_filtered.setSize(filter_size)
+for i in range(filter_size): # probably a weighting
+    calib_vel_filtered.setElement(i, 1.0/float(filter_size))
+plug(robot.device.joint_velocities, calib_vel_filtered.sin)
 
-q[0] = 0.4
-q[1] = 0.8
-q[2] = -1.6
+plug(robot.device.joint_positions, leg_calibrator.raw_position)
+plug(calib_vel_filtered.sout, leg_calibrator.velocity)
 
-# Update the initial state of the robot.
-robot.reset_state(q, dq)
-robot.set_gravity((0.0,0.0,0.0))
-#### Calibrator setup
+leg_calibrator.calibration_torque.value = (-0.2, 0.1) # 8*[0.0,]
+# leg_calibrator.kp.value = 2*(0.3,)
+leg_calibrator.hardstop2zero.value = 2*(0.0,)
 
-joint_calibrator = Calibrator('joint_calibrator')
+######################################## Set up PD controller
 
-## plug inputs in
-plug(robot.device.joint_positions, joint_calibrator.rawPosition)
-plug(robot.device.joint_velocities, joint_calibrator.velocity)
-joint_calibrator.desiredVelocity.value = [0.3, 0] # 2*[0.0,]
-joint_calibrator.kp.value = 2*[8.0,]
-joint_calibrator.hardstop2zero.value = 2*[0.0,]
+pd = ControlPD("pd_ctrl")
+pd.Kp.value = (0.0, 1.0)
+pd.Kd.value = (0.0, 0.01)
 
-#### create a switch for the output torque
+pd.desiredposition.value = 2*(0.0,)
+pd.desiredvelocity.value = 2*(0.0,)
+
+plug(leg_calibrator.calibrated_position, pd.position)
+plug(calib_vel_filtered.sout, pd.velocity)
+
+##################### SWITCH between controllers
 
 control_switch = SwitchVector("control_switch")
 control_switch.setSignalNumber(2) # we want to switch between 2 signals
-plug(joint_calibrator.control, control_switch.sin0)
-# plug(pd.control, control_switch.sin1)
-control_switch.sin1.value = [0.0,0.0]
-control_switch.selection.value = 0 # pick and switch manually for now
+plug(leg_calibrator.control, control_switch.sin0)
 
+# control_switch.sin1.value = (0.0, 0.0) # 8*[0.0,]
+plug(pd.control, control_switch.sin1)
+
+control_switch.selection.value = 0 # pick and switch manually for now
 plug(control_switch.sout, robot.device.ctrl_joint_torques)
 
-#################################################################################
+###################### Record Data ##########################################################
+
+# leg_imp_ctrl.record_data(robot)
+
+robot.add_trace("leg_calibrator", "calibrated_position")
+robot.add_ros_and_trace("leg_calibrator", "calibrated_position")
+
+robot.add_trace("calib_vel_filtered", "sout")
+robot.add_ros_and_trace("calib_vel_filtered", "sout")
+
+robot.add_trace("pd_ctrl", "control")
+robot.add_ros_and_trace("pd_ctrl", "control")
+
+# robot.add_trace("hopper_des_position", "sout")
+# robot.add_ros_and_trace("hopper_des_position", "sout")
+
+# robot.add_trace("hopper_des_velocity", "sout")
+# robot.add_ros_and_trace("hopper_des_velocity", "sout")
+
+# robot.add_trace("slider_fir_filter", "sout")
+# robot.add_ros_and_trace("slider_fir_filter", "sout")
