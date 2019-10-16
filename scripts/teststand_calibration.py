be9925091fcbcd1e03416981c62c6fec130afe98
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-10-16 13:20:25 +0200

    Huge clean in the files

--- scripts/teststand_calibration.py
+++
@@ -1,9 +0,0 @@
-import sys
-from dg_blmc_robots.calibration import calibrate
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        mechanical_calibration = bool(sys.argv[1])
-    else:
-        print("Please indicate if you want a mechanical calibration or not.")
-    calibrate(mechanical_calibration)
