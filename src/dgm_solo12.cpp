5a1ed4b28fb598258a2ade150df19b3e04bfca5f
Julian Viereck <jviereck@tuebingen.mpg.de>
2020-11-05 13:52:44 +0100

    Solo12: Add more safety checks and logging

--- src/dgm_solo12.cpp
+++ src/dgm_solo12.cpp
@@ -54,48 +54,66 @@ void DGMSolo12::initialize_hardware_communication_process()
 
   bool DGMSolo12::is_in_safety_mode()
   {
-    // Check for too fast velocity. Value estiamted from small jump.
-    if (solo_.get_joint_velocities().cwiseAbs().maxCoeff() > 60.) 
-    {
+    // Check if any card is in an error state.
+    if (solo_.has_error()) {
       was_in_safety_mode_ = true;
       static int counter = 0;
       if (counter % 2000 == 0) {
+        printf("DGMSolo12: Going into safe mode as motor card reports error.\n");
+      }
+      counter += 1;
+    }
+
+    // Check for too fast velocity. Value estiamted from small jump.
+    if (solo_.get_joint_velocities().cwiseAbs().maxCoeff() > 80.)
+    {
+      was_in_safety_mode_ = true;
+      static int counter = 0;
+      if (counter % 2000 == 0)
+      {
         printf("DGMSolo12: Going into safe mode as joint velocity exceeded bound.\n");
       }
       counter += 1;
     }
 
-    // Check for joint limits.
-    Eigen::Array<double, 12, 1> lower_lim, upper_lim;
-    lower_lim << -1.5, -1.5, -3., -1.5, -1.5, -3,
-                 -1.5, -1.5, -3., -1.5, -1.5, -3;
-    upper_lim = -lower_lim;
+    // Check for joint limits. Only do this when calibration is not running.
+    if (!solo_.is_calibrating()) {
+      Eigen::Array<double, 12, 1> lower_lim, upper_lim;
+      lower_lim << -1.2, -1.7, -3.4, -1.2, -1.7, -3.4,
+                   -1.2, -1.7, -3.4, -1.2, -1.7, -3.4;
+      upper_lim = -lower_lim;
 
-    if ((solo_.get_joint_positions().array() < lower_lim).any())
-    {
-      was_in_safety_mode_ = true;
-      static int counter = 0;
-      if (counter % 2000 == 0) {
-        printf("DGMSolo12: below joint limits.\n");
+      if ((solo_.get_joint_positions().array() < lower_lim).any())
+      {
+        was_in_safety_mode_ = true;
+        static int counter = 0;
+        if (counter % 2000 == 0)
+        {
+          printf("DGMSolo12: below joint limits.\n");
+          std::cout << solo_.get_joint_positions().transpose() << std::endl;
+        }
+        counter += 1;
       }
-      counter += 1;
-    }
 
-    if ((solo_.get_joint_positions().array() > upper_lim).any())
-    {
-      was_in_safety_mode_ = true;
-      static int counter = 0;
-      if (counter % 2000 == 0) {
-        printf("DGMSolo12: above joint limits.\n");
+      if ((solo_.get_joint_positions().array() > upper_lim).any())
+      {
+        was_in_safety_mode_ = true;
+        static int counter = 0;
+        if (counter % 2000 == 0)
+        {
+          printf("DGMSolo12: above joint limits.\n");
+          std::cout << solo_.get_joint_positions().transpose() << std::endl;
+        }
+        counter += 1;
       }
-      counter += 1;
     }
 
     if (was_in_safety_mode_ || DynamicGraphManager::is_in_safety_mode())
     {
       static int counter = 0;
       was_in_safety_mode_ = true;
-      if (counter % 2000 == 0) {
+      if (counter % 2000 == 0)
+      {
         printf("DGMSolo12: is_in_safety_mode.\n");
       }
       counter++;
@@ -107,25 +125,19 @@ void DGMSolo12::initialize_hardware_communication_process()
   {
     // Check if there is an error with the motors. If so, best we can do is
     // to command zero torques.
-    dynamicgraph::Vector& map_motor_board_errors = sensors_map_.at("motor_board_errors");
-    for(size_t i=0 ; i<map_motor_board_errors.size() ; ++i)
-    {
-      // Check if there is an error. In that case, send zero torques.
-      if (map_motor_board_errors[i] != 0) {
-        for (auto ctrl = motor_controls_map_.begin();
-            ctrl != motor_controls_map_.end();
-            ++ctrl)
-        {
-            ctrl->second.fill(0.0);
-        }
-        return;
+    if (solo_.has_error()) {
+      for (auto ctrl = motor_controls_map_.begin();
+          ctrl != motor_controls_map_.end();
+          ++ctrl)
+      {
+          ctrl->second.fill(0.0);
       }
+    } else {
+      // The motors are fine.
+      // --> Run a D controller to damp the current motion.
+      motor_controls_map_.at("ctrl_joint_torques") =
+          -0.05 * sensors_map_.at("joint_velocities");
     }
-
-    // The motors are fine.
-    // --> Run a D controller to damp the current motion.
-    motor_controls_map_.at("ctrl_joint_torques") =
-        -0.05 * sensors_map_.at("joint_velocities");
   }
 
   void DGMSolo12::get_sensors_to_map(dynamic_graph_manager::VectorDGMap& map)
